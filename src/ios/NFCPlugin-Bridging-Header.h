ad243650ff056dac4d532952cbaedba9cce1bf53
andychen00 <andychen9979@gmail.com>
2026-01-20 13:36:35 +0700

    t

--- src/ios/NFCPlugin-Bridging-Header.h
+++ src/ios/NFCPlugin-Bridging-Header.h
@@ -6,5 +6,11 @@
 
 // #import "../../Classes/AppDelegate.h"
 
+// File: NFCPlugin-Bridging-Header.h
+// HAPUS SEMUA, GANTI DENGAN:
+#ifndef NFCPlugin_Bridging_Header_h
+#define NFCPlugin_Bridging_Header_h
+
 #import <Cordova/CDV.h>
-#import <CoreNFC/CoreNFC.h>
+
+#endif
